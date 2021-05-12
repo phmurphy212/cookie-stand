aae1ee0eb72b1422b12b704ddb2e931f1a1e5e54
Phil Murphy <phillipdeanmurphy@gmail.com>
2021-05-12 15:30:24 -0700

    cleaned up comments

--- js/app.js
+++ js/app.js
@@ -3,13 +3,14 @@ function salmonCookies() {
   let hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'];
 
   //proof of life
-  console.log('Hello Squirrel');
+  // console.log('Hello Squirrel');
 
-  //trying to manipulate the dom
+  //overall function for seattle cookie functions
   function seattleCookieFunction() {
+    //access the dom
     const seattleList = document.getElementById('seattle-list');
+    //
     //object literal with the Seattle data
-
     let seattleStore = {
       name: 'Seattle',
       min: 23,
@@ -17,40 +18,48 @@ function salmonCookies() {
       dailyTotal: 0,
       avgCookiesPerHourArray: [],
       avg: 6.3,
+      //function to return a random number
       randomCustCount: function () {
         return Math.floor(Math.random() * (this.max - this.min + 1) + this.min);
       },
+      //function to estimate hourly sales rounded up
       cookieSales: function () {
         return Math.ceil(this.avg * this.randomCustCount());
       },
+      //function to move hourly sales from previous function into an array
       cookiesToArray: function () {
         for (let i = 0; i < hours.length; i++) {
           this.avgCookiesPerHourArray.push(this.cookieSales());
         }
       },
+      //function to add up the hourly sales from array
       sumOfSales: function () {
         for (let i = 0; i < this.avgCookiesPerHourArray.length; i++) {
           this.dailyTotal += this.avgCookiesPerHourArray[i];
         }
       },
+      //push sales to DOM
       renderToSales: function () {
         for (let i = 0; i < hours.length; i++) {
           let li = document.createElement('li');
           li.textContent = `${hours[i]}: ${this.cookieSales()} cookies`;
           seattleList.appendChild(li);
         }
+        //push total to DOM
         let liTotal = document.createElement('li');
         liTotal.textContent = `Total: ${this.dailyTotal} cookies`;
         seattleList.appendChild(liTotal);
       },
+      //call sub-functions to return data
     };
     seattleStore.cookiesToArray();
     seattleStore.sumOfSales();
     seattleStore.renderToSales();
   }
+  //the rest of the city based stores follow the same function pattern as referenced
+  //in the seattleCookieFunction so I'm leaving off the comments
   function tokyoCookieFunction() {
     const tokyoList = document.getElementById('tokyo-list');
-    //object literal with the Tokyo data
 
     let tokyoStore = {
       name: 'tokyo',
@@ -92,7 +101,6 @@ function salmonCookies() {
   }
   function dubaiCookieFunction() {
     const dubaiList = document.getElementById('dubai-list');
-    //object literal with the Dubai data
 
     let dubaiStore = {
       name: 'Dubai',
@@ -134,7 +142,7 @@ function salmonCookies() {
   }
   function parisCookieFunction() {
     const parisList = document.getElementById('paris-list');
-    //object literal with the Paris data
+
     let parisStore = {
       name: 'Paris',
       min: 20,
@@ -175,7 +183,7 @@ function salmonCookies() {
   }
   function limaCookieFunction(){
     const limaList = document.getElementById('lima-list');
-    //object literal with the Lima data
+
     let limaStore = {
       name: 'Lima',
       min: 2,
@@ -214,6 +222,8 @@ function salmonCookies() {
     limaStore.sumOfSales();
     limaStore.renderToSales();
   }
+  //the only purpose of the 'salmon cookies' function is to call each of the
+  //functions within it so later I can just call a single function
   seattleCookieFunction();
   tokyoCookieFunction();
   dubaiCookieFunction();
